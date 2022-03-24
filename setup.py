9f120f40d1584fd52aab63288af3c6e8e57102b3
Quentin Labourey <quentin.labourey@gmail.com>
2022-03-24 20:03:36 +0100

    First version of package - NoXacro

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import os
+from glob import glob
+
+
+package_name = 'robot_urdf'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+  	    (os.path.join('share', package_name), glob('launch/*.py')),
+  	    (os.path.join('share', package_name), glob('urdf/*'))
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='quentin',
+    maintainer_email='quentin.labourey@gmail.com',
+    description='URDF description of the robot through XACRO file',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'state_publisher=robot_urdf.joint_publisher:main'
+        ],
+    },
+)
